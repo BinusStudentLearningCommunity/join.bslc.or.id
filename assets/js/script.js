a31b57b8fd525be27fc34c1971ad3b513e69a2d9
Aradhana Indra D(ev) <aradhanaindradev@gmail.com>
2022-07-23 16:40:27 +0700

    Fixed Responsive

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function linktoMainWebsite(){
+    window.open("http://bslc.or.id", '_blank')
+}
+
+function registerMentor(){
+    window.open("https://bit.ly/mentorbslcrecruitmentganjil", '_blank')
+}
